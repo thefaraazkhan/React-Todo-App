18061181ee677e047760262a8554ecd8d9744051
thefaraazkhan <khanmohdfaraaz@gmail.com>
2023-05-15 20:33:19 +0530

    fixed server url again

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -1,4 +1,14 @@
-export const host = "http://localhost:8000";
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
+const dev = process.env.NODE_ENV !== 'production';
+
+export const host = dev
+  ? 'http://localhost:8000'
+  : 'https://swapify-backend.onrender.com';
+
+console.log(dev);
+
 // export const loginRoute = `${host}/api/auth/login`;
 // export const registerRoute = `${host}/api/auth/register`;
 // export const logoutRoute = `${host}/api/auth/logout`;
